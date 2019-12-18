7f6070dbedf0db0e6d89ad9b5dd1f4dcc9f4d79b
Dai Kawahara <skbsporta7@gmail.com>
2019-12-18 23:58:48 +0900

    Initial Commit

---
+++ RxSwift-GCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwift-GCD
+//
+//  Created by kawaharadai on 2019/12/18.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RxSwift-GCDTests/RxSwift_GCDTests.swift
@@ -0,0 +1,34 @@
+//
+//  RxSwift_GCDTests.swift
+//  RxSwift-GCDTests
+//
+//  Created by kawaharadai on 2019/12/18.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+import XCTest
+@testable import RxSwift_GCD
+
+class RxSwift_GCDTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
